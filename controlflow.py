dceb714fda559bfdae1ab95fd012490e00df44a0
kriviit <kriviitbigdata@gmail.com>
2026-02-11 21:16:30 +0530

    day-1 and day-2

---
+++ controlflow.py
@@ -0,0 +1,57 @@
+a=10
+print(a)
+b,c=20,30
+print(b)
+print(c)
+d=i=f=20
+print(d)
+print(i)
+print(f)
+
+
+a="python"
+b=20
+c=40.66
+d=True
+f=3+6j
+# type()->datatype or class name of the object
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(f))
+
+
+
+#dsa
+# range
+a=range(10)
+print(type(a))
+print(a)
+print(list(a))
+print(list(range(1,20)))
+print(list(range(1,20,2)))
+print(list(range(0,20,2)))
+
+## list
+a=[10,20,30,40] # 
+print(type(a))
+print(a)
+
+a=(10,20,40,50,60)
+print(type(a))
+print(a)
+print(type(a))
+print(a)
+
+
+a={1,2,3,4,5,6}
+print(type(a))
+print(a)
+
+
+#json key:value
+
+data={"name":"python"}
+print(type(data))
+print(data)
